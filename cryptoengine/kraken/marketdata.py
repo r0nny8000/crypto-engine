54754b6a0ba30e476ff0689c9eb49f99e977eb27
r0nny8000 <r0nny8000@bea2b.de>
2025-02-04 22:13:39 +0100

    Refactor logging configuration in marketdata module to use a constant for log format

--- cryptoengine/kraken/marketdata.py
+++ cryptoengine/kraken/marketdata.py
@@ -9,7 +9,8 @@ from kraken.spot import Market
 from kraken.exceptions import * # pylint: disable=wildcard-import,unused-wildcard-import
 
 
-logging.basicConfig(level=logging.WARNING, format='%(asctime)s %(levelname)-8s %(funcName)-16s %(message)s', filename='/tmp/crypto-engine.log')
+LOGGING_FORMAT = "%(asctime)s %(levelname)-8s %(funcName)-16s %(message)s"
+logging.basicConfig(level=logging.WARNING, format=LOGGING_FORMAT, filename="/tmp/crypto-engine.log")
 
 def get_ticker(pair):
     """
